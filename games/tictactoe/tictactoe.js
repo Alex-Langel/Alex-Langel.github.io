4e24a3dbca06dfed23215d7c5d6049816191874f
Alex-Langel <alexander.langel98@gmail.com>
2025-03-31 13:48:09 -0400

    Add TicTacToe AI

--- games/tictactoe/tictactoe.js
+++ games/tictactoe/tictactoe.js
@@ -8,6 +8,7 @@ let lineSize = canvasWidth / 50;
 let turn = 0;
 var gameOver = 0;
 let board = [[0,0,0],[0,0,0],[0,0,0]];
+let posMoves = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
 let gameState = 0;
 let humanGame = true;
 
@@ -17,8 +18,6 @@ function setup() {
     let canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent("canvCont");
     fill(25,0,50);
-    console.log(canvasWidth);
-    console.log(cellSize);
     rect(0,0,canvasWidth, canvasHeight);
     drawBoard();
     drawControls();
@@ -28,16 +27,15 @@ function getStartingDims() {
     let windowHeight = container.clientHeight;
     let newWidth = (windowWidth);
     let newHeight = (windowHeight);
-    newHeight = Math.max(newHeight, 125);
+    newHeight = Math.max(newHeight, 120);
     newWidth = Math.max(newWidth, 100);
     if (newWidth > newHeight * (8/10)) {
         newWidth = newHeight * (8/10);
     } else {
-        newHeight = newWidth * (12/10);
+        newHeight = newWidth * (10/8);
     }
     canvasHeight = newHeight
     canvasWidth = newWidth
-    console.log(newHeight);
     lineSize = canvasWidth / 50;
     cellSize = canvasWidth / 3;
 }
@@ -49,19 +47,19 @@ function drawControls() {
         strokeWeight(lineSize);
         stroke(0,0,0);
         fill(50,0,100);
-        rect(0,height*(8/10),width/2,height*(2/10));
-        drawCenteredText("vs Human",0,height*(8/10),width/2,height*(2/10), 80);
+        rect(0,canvasHeight*(8/10),canvasWidth/2,canvasHeight*(2/10));
+        drawCenteredText("vs Human",0,canvasHeight*(8/10),canvasWidth/2,canvasHeight*(2/10), 80);
         strokeWeight(lineSize);
         stroke(0,0,0);
         fill(100,0,200);
-        rect(width/2,height*(8/10),width/2,height*(2/10));
-        drawCenteredText(" vs CPU", width/2,height*(8/10),width/2,height*(2/10), 80);
+        rect(canvasWidth/2,canvasHeight*(8/10),canvasWidth/2,canvasHeight*(2/10));
+        drawCenteredText("vs CPU", canvasWidth/2,canvasHeight*(8/10),canvasWidth/2,canvasHeight*(2/10), 80);
     } else {
         strokeWeight(lineSize);
         stroke(0,0,0);
         fill(75,0,150);
-        rect(0,height*(8/10),width,height*(2/10));
-        drawCenteredText("Reset",0,height*(8/10),width,height*(2/10), 160);
+        rect(0,canvasHeight*(8/10),canvasWidth,canvasHeight*(2/10));
+        drawCenteredText("Reset",0,canvasHeight*(8/10),canvasWidth,canvasHeight*(2/10), 160);
     }
 }
 function drawEmptyBoard() {
@@ -82,7 +80,6 @@ function drawBoard() {
 
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[i].length; j++) {
-            console.log(board[i][j]);
             switch (board[i][j]) {
                 case 0:
                     strokeWeight(lineSize);
@@ -111,8 +108,6 @@ function drawBoard() {
 }
 function mouseClicked() {
     var gridHeight = canvasHeight * (8/10);
-    console.log(mouseX, mouseY);
-    console.log(canvasWidth, gridHeight);
     if (mouseY > 0 && mouseY < gridHeight * (1/3)) {
         if (mouseX > 0 && mouseX < canvasWidth * (1/3)) {
             clickCell(0,0);
@@ -148,7 +143,7 @@ function mouseClicked() {
             if (mouseX > 0 && mouseX < canvasWidth / 2) {
                 startGameHuman();
             } else if (mouseX > canvasWidth / 2 && mouseX < canvasWidth) {
-                //startGameComputer();
+                startGameComputer();
             } else {
                 console.log("mouse X Value O.O.B.")
             }
@@ -177,11 +172,194 @@ function clickCell(row, col) {
                 }
             }
         } else {//CPU GAME
-
+            if (turn % 2 == 0) {
+                if (board[row][col] == 0) {
+                    board[row][col] = plValue;
+                    posMoves.splice(getArrayIndex(row,col),1);
+                    console.log(posMoves);
+                    drawBoard();
+                    checkForWin();
+                    turn++;
+                    if (gameState == 1 && turn == 9) {
+                        drawBoard();
+                    } else if (gameState == 1) {
+                        cpuTurn();
+                    }
+                }
+            }
         }
     }
 
 }
+
+function getArrayIndex(row, col) {
+    for (var i = 0; i < posMoves.length; i++) {
+        if (posMoves[i][0] == row && posMoves[i][1] == col) {
+            console.log(i);
+            return i;
+        }
+    }
+}
+
+function cpuTurn() {
+    var cpuNextCoords = getCPUMove();
+    console.log(cpuNextCoords);
+    posMoves.splice(getArrayIndex(cpuNextCoords[0],cpuNextCoords[1]),1);
+    board[cpuNextCoords[0]][cpuNextCoords[1]] = 1;
+    console.log(posMoves);
+    drawBoard();
+    checkForWin();
+    turn++;
+    if (gameState == 1 && turn == 9) {
+        drawBoard();
+    }
+}
+
+function getCPUMove(){
+    var cpuNextCoords = findWin();
+    if (cpuNextCoords[0] != -1 && cpuNextCoords[1] != -1) {
+        console.log("CPU Winning");
+        return cpuNextCoords;
+    } else {
+        cpuNextCoords = findBlock();
+        if (cpuNextCoords[0] != -1 && cpuNextCoords[1] != -1) {
+            console.log("CPU Blocking");
+            return cpuNextCoords;
+        } else {
+            if (board[1][1] == "0") {
+                console.log("CPU Taking Center");
+                return [1,1]
+            } else {
+                console.log("CPU Going Randomly");
+                shuffleArray(posMoves);
+                return [posMoves[0][0], posMoves[0][1]];
+            }
+        }
+    }
+}
+
+function findWin() {
+    var checkInt1;
+    var checkInt2;
+    var checkInt3;
+
+    //Row checking
+    for (var i = 0; i < 3; i++) {
+        checkInt1 = board[i][0];
+        checkInt2 = board[i][1];
+        checkInt3 = board[i][2];
+
+        if (checkInt1 == 1 && checkInt2 == 1 && checkInt3 == 0) {
+            return [i,2];
+        } else if (checkInt2 == 1 && checkInt3 == 1 && checkInt1 == 0) {
+            return [i,0];
+        } else if (checkInt3 == 1 && checkInt1 == 1 && checkInt2 == 0) {
+            return [i,1];
+        }
+    }
+    //Col checking
+    for (var i = 0; i < 3; i++) {
+        checkInt1 = board[0][i];
+        checkInt2 = board[1][i];
+        checkInt3 = board[2][i];
+        if (checkInt1 == 1 && checkInt2 == 1 && checkInt3 == 0) {
+            return [2,i];
+        } else if (checkInt2 == 1 && checkInt3 == 1 && checkInt1 == 0) {
+            return [0,i];
+        } else if (checkInt3 == 1 && checkInt1 == 1 && checkInt2 == 0) {
+            return [1,i];
+        }
+    }
+    //Diag checking
+    checkInt1 = board[0][0];
+    checkInt2 = board[1][1];
+    checkInt3 = board[2][2];
+    if (checkInt1 == 1 && checkInt2 == 1 && checkInt3 == 0) {
+        return [2,2];
+    } else if (checkInt2 == 1 && checkInt3 == 1 && checkInt1 == 0) {
+        return [0,0];
+    } else if (checkInt3 == 1 && checkInt1 == 1 && checkInt2 == 0) {
+        return [1,1];
+    }
+    checkInt1 = board[0][2];
+    checkInt2 = board[1][1];
+    checkInt3 = board[2][0];
+    if (checkInt1 == 1 && checkInt2 == 1 && checkInt3 == 0) {
+        return [2,0];
+    } else if (checkInt2 == 1 && checkInt3 == 1 && checkInt1 == 0) {
+        return [0,2];
+    } else if (checkInt3 == 1 && checkInt1 == 1 && checkInt2 == 0) {
+        return [1,1];
+    }
+    return [-1,-1];
+}
+
+function findBlock() {
+    var checkInt1;
+    var checkInt2;
+    var checkInt3;
+
+    //Row checking
+    for (var i = 0; i < 3; i++) {
+        checkInt1 = board[i][0];
+        checkInt2 = board[i][1];
+        checkInt3 = board[i][2];
+
+        if (checkInt1 == 2 && checkInt2 == 2 && checkInt3 == 0) {
+            return [i,2];
+        } else if (checkInt2 == 2 && checkInt3 == 2 && checkInt1 == 0) {
+            return [i,0];
+        } else if (checkInt3 == 2 && checkInt1 == 2 && checkInt2 == 0) {
+            return [i,1];
+        }
+    }
+    //Col checking
+    for (var i = 0; i < 3; i++) {
+        checkInt1 = board[0][i];
+        checkInt2 = board[1][i];
+        checkInt3 = board[2][i];
+        if (checkInt1 == 2 && checkInt2 == 2 && checkInt3 == 0) {
+            return [2,i];
+        } else if (checkInt2 == 2 && checkInt3 == 2 && checkInt1 == 0) {
+            return [0,i];
+        } else if (checkInt3 == 2 && checkInt1 == 2 && checkInt2 == 0) {
+            return [1,i];
+        }
+    }
+    //Diag checking
+    checkInt1 = board[0][0];
+    checkInt2 = board[1][1];
+    checkInt3 = board[2][2];
+    if (checkInt1 == 2 && checkInt2 == 2 && checkInt3 == 0) {
+        return [2,2];
+    } else if (checkInt2 == 2 && checkInt3 == 2 && checkInt1 == 0) {
+        return [0,0];
+    } else if (checkInt3 == 2 && checkInt1 == 2 && checkInt2 == 0) {
+        return [1,1];
+    }
+    checkInt1 = board[0][0];
+    checkInt2 = board[1][1];
+    checkInt3 = board[2][2];
+    if (checkInt1 == 2 && checkInt2 == 2 && checkInt3 == 0) {
+        return [2,2];
+    } else if (checkInt2 == 2 && checkInt3 == 2 && checkInt1 == 0) {
+        return [0,0];
+    } else if (checkInt3 == 2 && checkInt1 == 2 && checkInt2 == 0) {
+        return [1,1];
+    }
+    checkInt1 = board[0][2];
+    checkInt2 = board[1][1];
+    checkInt3 = board[2][0];
+    if (checkInt1 == 2 && checkInt2 == 2 && checkInt3 == 0) {
+        return [2,0];
+    } else if (checkInt2 == 2 && checkInt3 == 2 && checkInt1 == 0) {
+        return [1,1];
+    } else if (checkInt3 == 2 && checkInt1 == 2 && checkInt2 == 0) {
+        return [0,2];
+    }
+    return [-1,-1];
+}
+
 function startGameHuman() {
     console.log("HUMAN GAME");
     humanGame = true;
@@ -199,6 +377,7 @@ function resetGame() {
     gameState = 0;
     turn = 0;
     board = [[0,0,0],[0,0,0],[0,0,0]];
+    posMoves = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
     drawControls();
     drawBoard();
 }
@@ -281,143 +460,14 @@ function checkForWin() {
     }    
 }
 function threeInRow(fNum, sNum, tNum) {
-    console.log(fNum, sNum, tNum);
     if (fNum == sNum && sNum == tNum && tNum == fNum && fNum != 0) {
-        console.log(fNum, sNum, tNum);
         return true;
     }
     else {
         return false;
     }
 }
-
-function btnFun(button) {
-const buttn = button;
-const rect = buttn.getBoundingClientRect();
-const top = rect.top - 9; // Distance from the top of the viewport
-const left = rect.left - 9; // Distance from the left edge of the viewport
-const right = left + 100; // Distance from the right edge of the viewport
-const bottom = top + 100; // Distance from the bottom of the viewport
-var tRow, tCol;
-if (gameOver != 0) {
-    return;
-}
-else {
-    if (turn % 2 == 0) {//even turn
-        line(left,top,right,bottom);//X
-        line(left,bottom,right,top);//X
-        document.getElementById("output").innerText = "Player 2's Turn";
-    } else {//odd turn
-        ellipse(left+50,top+50,100,100);//O
-        document.getElementById("output").innerText = "Player 1's Turn";
-    }
-    button.style.visibility = "hidden";//hide button pressed
-
-    switch (top) { //col
-        case 0:
-            tCol = 0;
-            break;
-        case 100:
-            tCol = 1;
-            break;
-        case 200:
-            tCol = 2;
-            break;
-    }
-    switch(left) {//row
-        case 0:
-            tRow = 0;
-            break;
-        case 100:
-            tRow = 1;
-            break;
-        case 200:
-            tRow = 2;
-            break;
-    }
-    updateBoard(tRow, tCol);
-    chkWin();
-    turn++;
-    if (turn > 8 && gameOver == 0) {
-        endGame("Tie Game");
-    }
-}
-}
-
-function updateBoard(row, col) {
-    if (turn % 2 == 0) {
-        board[row][col] = 1;
-    } else {
-        board[row][col] = 2;
-    } 
-    return;
-}
-
-function chkWin() {
-    for (var i = 0; i < 3; i++) {
-        if (board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1) { //any col X
-        stroke('red');
-        strokeWeight(5);
-        line((i*100)+50, 0, (i*100)+50, 300);
-        endGame("Player 1 Wins");
-        } else if (board[i][0] == 2 && board[i][1] == 2 && board[i][2] == 2) { //any col O
-        stroke('red');
-        strokeWeight(5);
-        line((i*100)+50, 0, (i*100)+50, 300);
-        endGame("Player 2 Wins");
-        } else if (board[0][i] == 1 && board[1][i] == 1 && board[2][i] == 1) { //any row X
-        stroke('red');
-        strokeWeight(5);
-        line(0, (i*100)+50, 300, (i*100)+50);
-        endGame("Player 1 Wins");
-        } else if (board[0][i] == 2 && board[1][i] == 2 && board[2][i] == 2) { //any row O
-        stroke('red');
-        strokeWeight(5);
-        line(0, (i*100)+50, 300, (i*100)+50);
-        endGame("Player 2 Wins");
-        } 
-    } 
-    if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) { //first diag X
-        stroke('red');
-        strokeWeight(5);
-        line(0, 0, 300, 300);
-        endGame("Player 1 Wins");
-    } else if (board[0][2] == 1 && board[1][1] == 1 && board[2][0] == 1) { //other diag X
-        stroke('red');
-        strokeWeight(5);
-        line(0, 300, 300, 0);
-        endGame("Player 1 Wins");
-    } else if (board[0][0] == 2 && board[1][1] == 2 && board[2][2] == 2) { //first diag O
-        stroke('red');
-        strokeWeight(5);
-        line(0, 0, 300, 300);
-        endGame("Player 2 Wins");
-    } else if (board[0][2] == 2 && board[1][1] == 2 && board[2][0] == 2) { //other diag O
-        stroke('red');
-        strokeWeight(5);
-        line(0, 300, 300, 0);
-        endGame("Player 2 Wins");
-    } 
-    stroke('black');
-    strokeWeight(1);
-    return;
-}
-function delBtns() {
-    var myDiv = document.querySelector(".canvas-container");
-    while (myDiv.lastChild != myCanvas) {
-        myDiv.removeChild(myDiv.lastChild);
-    }
-    return;
-}
-function resetFunc() {
-    delBtns();
-    secSetup();
-}
-function endGame(strOut) {
-    gameOver = 1;
-    document.getElementById("output").innerText = strOut;
-}
-
+//Util
 function drawCenteredText(displayText, left, top, width, height, txtSize) {
     let txtMaxHi = height * (9/10);
     let txtMaxWi = width * (9/10);
@@ -444,4 +494,19 @@ function drawCenteredText(displayText, left, top, width, height, txtSize) {
     stroke(textColor);
     textAlign(CENTER, CENTER);
     text(displayText, left + width / 2, top + height / 2);
+}
+function shuffleArray(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
 }
